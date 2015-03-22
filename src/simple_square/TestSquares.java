2281ac3163401e3af1f5497d5e74622a37772992
Agus <agus@agus.com>
2015-03-22 16:12:40 -0300

    maven andando

--- src/simple_square/TestSquares.java
+++ src/simple_square/TestSquares.java
@@ -3,12 +3,36 @@ package simple_square;
 import gps.GPSEngine;
 import gps.SearchStrategy;
 
+import java.util.Scanner;
+
 public class TestSquares {
 	public static void main(String[] args) {
 		long time = System.currentTimeMillis();
 		GPSEngine engine = new SimpleSquaresEngine();
 		
-		engine.engine(new SimpleSquaresProblem(SearchStrategy.Greedy), SearchStrategy.Greedy);
+		Scanner s = new Scanner(System.in);
+		while(true){
+		System.out.println("Ingrese una estrategia:");
+		String strat = s.nextLine();
+		SearchStrategy strategy = SearchStrategy.BFS;
+		if(strat.toLowerCase().equals("bfs")){
+			strategy = SearchStrategy.BFS;
+		}else if(strat.toLowerCase().equals("dfs")){
+			strategy = SearchStrategy.DFS;
+		}else if(strat.toLowerCase().equals("astar")){
+			strategy = SearchStrategy.AStar;
+		}else if(strat.toLowerCase().equals("deepiteration")){
+			strategy = SearchStrategy.DeepIteration;
+		}else if(strat.toLowerCase().equals("greedy")){
+			strategy = SearchStrategy.Greedy;
+		}else if(strat.toLowerCase().equals("quit")){
+			return;
+		}else{
+			System.out.println("No se encontró estrategia con ese nombre");
+		}
+		
+		engine.engine(new SimpleSquaresProblem(strategy), strategy);
 		System.out.println("Total time in milis: " + ((System.currentTimeMillis() - time)));
+		}
 	}
 }
