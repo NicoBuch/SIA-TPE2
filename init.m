5cf541e1c45f31af9001b2d3f8fc7e2f9075ec64
Francisco Depascuali <francisco.depascuali@gmail.com>
2015-05-02 21:07:47 -0300

    con mas cantidad en las esquinas

--- init.m
+++ init.m
@@ -1,13 +1,13 @@
 more off;
-load ourFunctionIn5.txt values;
-layerSizes = [1 20 15 10];
+load puntosEsquinas10.txt values;
+layerSizes = [1 40 30 20 15 20 30 40];
 eta = 0.1;
 gValue = 1;
 betaValue = 0.5;
 error = 1;
-momentum = 0.5;
-etaAdaptativo = 5;
-a = 0.05;
+momentum = 0;
+etaAdaptativo = 0;
+a = 0.01;
 b = 0.1;
 W = initializeWeights(layerSizes);
 multiLayerPerceptron(W,values, layerSizes, eta, gValue, betaValue, error, momentum, etaAdaptativo, a, b);
