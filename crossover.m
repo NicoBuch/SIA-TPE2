d4caaed6bc67c2ff117a198ebbf7c25485cec702
NicoBuch <nicolas.buchhalter@wolox.com.ar>
2015-05-24 20:07:24 -0300

    fix

--- crossover.m
+++ crossover.m
@@ -2,7 +2,8 @@ function children = crossover(people, cross_probability, cross_function)
   while(length(children) < length(people))
     indexes = randi(length(people), 1, 2)
     if(rand <= cross_probability)
-      children = cross_function(people{indexes(1)}, people{indexes(2)});
+      new_children = cross_function(people{indexes(1)}, people{indexes(2)});
+      children = {children, new_children{1}, new_children{2}};
     endif
   endwhile
 endfunction
